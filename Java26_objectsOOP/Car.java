b36baf10324ccbda511cfa7fb531b4c0e7912c2e
pminasz <pminasz@gmail.com>
2024-03-22 00:00:35 +0100

    JavaFullCourseForFree

--- Java26_objectsOOP/Car.java
+++ Java26_objectsOOP/Car.java
@@ -1,4 +1,31 @@
 package Java26_objectsOOP;
 
 public class Car {
+        //attributes - model
+//    String make = "Chevrolet";
+//    String model = "Corvette";
+//    int year = 2020;
+//    String color = "blue";
+//    double price = 50000.00;
+    String make;
+    String model;
+    int year;
+    String color;
+    double price;
+
+    //constructor:
+    Car(String make, String model, int year, String color, double price) {
+        this.make = make;
+        this.model = model;
+    }
+
+    // methods (what actions the Car object can perform
+    void drive() {
+        System.out.println("you drive a car: "+ this.make);
+    }
+    void brake() {
+        System.out.println("you step on the brakes");
+    }
+
+
 }
--- Java26_objectsOOP/Main.java
+++ Java26_objectsOOP/Main.java
@@ -5,6 +5,33 @@ public class Main {
         //object = an instance of class that may contain attributes and methods
         //example: (phone, desk, computer, coffee cup)
         // for example, the characteristics (attributes) of coffee cup: String color =""; double temp =20.0; boolean empty = true;
-        //methods : drink(0; spill();
+        //methods : drink(); spill();
+
+
+        //constructing an instance of another class
+        //1. construct a Car object
+        //2. create a unique name of this instance of the class. this unique nam of the car
+        Car samochod = new Car("wolga", "RUS", 0 ,"null",0.0);
+        //3. display some characteristic of the Car
+        //3. type in the name of the Object(dot)attribute (here samochod.model)
+        System.out.println(samochod.make);
+        System.out.println(samochod.model);
+
+        // to display the method, it's the same process as for instances
+        //1. name of the object dot and the method I want to call
+        samochod.drive();
+        samochod.brake();
+
+        //it's possible to reuse the class to create multiple instances of the class,
+        //meaning that I can create a multiple objects
+        Car samochodzik = new Car("syrenka", "FSO", 0 ,null,0.0);
+        System.out.println(samochodzik.make);
+        System.out.println(samochodzik.model);
+
+        samochodzik.drive();
+        samochodzik.brake();
+
     }
+
+
 }
